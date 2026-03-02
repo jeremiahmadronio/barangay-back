2874f13db7879722a85a9e8c2a689335530beb20
jeremiahhhhhhhhhh <jeremiahmadronio2003@gmail.com>
2026-03-03 00:21:16 +0800

    refactor: fixing error in pom xml and removing unecessarry syntax error and removing duplicate package

--- src/main/java/com/barangay/barangay/dataseed/DataInitializer.java
+++ src/main/java/com/barangay/barangay/dataseed/DataInitializer.java
@@ -14,6 +14,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Component
@@ -66,7 +67,7 @@ public class DataInitializer implements CommandLineRunner {
             root.setLastName("Dela Cruz");
             root.setStatus(Status.ACTIVE);
             root.setRole(rootRole);
-            root.setDepartment(adminDept);
+            root.setAllowedDepartments(new HashSet<>(Set.of(adminDept)));
             root.setFailedAttempts(0);
             root.setIsLocked(false);
             userRepository.save(root);
